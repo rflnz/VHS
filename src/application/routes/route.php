ba30343fff26b39a798ca196ff7088632cbfd53c
____x_ <josuealexandre067@gmail.com>
2025-08-21 16:32:18 -0400

    Update route.php

--- src/application/routes/route.php
+++ src/application/routes/route.php
@@ -1,35 +1,36 @@
 <?php
 
-require_once __DIR__ . '/../../application/routes/route.config.php';
-require_once __DIR__ . '/../../vendor/routes.autoload.php';
-require_once __DIR__ . '/../../../vendor/autoload.php';
-require_once __DIR__ . '/../../controllers/signUp.controller.php';
-require_once __DIR__ . '/../../controllers/signIn.controller.php';
-require_once __DIR__ . '/../../controllers/signUpView.controller.php';
-require_once __DIR__ . '/../../controllers/createPassword.controller.php';
-require_once __DIR__ . '/../../controllers/home.controller.php';
-#require_once __DIR__ . '/../../controllers/verfiyEmail.controller.php';
-require_once __DIR__ . '/../../application/middlewares/RedirectUserLogged.middleware.php';
-require_once __DIR__ . '/../../application/middlewares/RedirectUserNotLogged.middleware.php';
-require_once __DIR__ . '/../../controllers/signIn.view.controller.php';
+   require_once __DIR__ . '/../../../vendor/autoload.php';
+   require_once __DIR__ . '/../../vendor/routes.autoload.php';
+   require_once __DIR__ . '/../../application/routes/route.config.php';
+   require_once __DIR__ . '/../../controllers/registerEvent.controller.php'; 
+   require_once __DIR__ . '/../../controllers/viewEvents.controller.php';
+// require_once __DIR__ . '/../../controllers/signUp.controller.php';
+// require_once __DIR__ . '/../../controllers/signIn.controller.php';
+// require_once __DIR__ . '/../../controllers/signUpView.controller.php';
+// require_once __DIR__ . '/../../controllers/createPassword.controller.php';
+// require_once __DIR__ . '/../../controllers/home.controller.php';
+// require_once __DIR__ . '/../../controllers/verfiyEmail.controller.php';
+// require_once __DIR__ . '/../../application/middlewares/RedirectUserLogged.middleware.php';
+// require_once __DIR__ . '/../../application/middlewares/RedirectUserNotLogged.middleware.php';
+// require_once __DIR__ . '/../../controllers/signIn.view.controller.php';
 
-use Dotenv\Dotenv;
-use Src\Application\Routes\Router;
-use Src\Application\Controllers\CreateUserController;
-use Src\Application\Controllers\SignUpController;
-use Src\Application\Controllers\SignUpViewController;
-use Src\Application\Controllers\CreatePasswordController;
-use Src\Application\Controllers\HomeController;
-use Src\Application\Controllers\VerifyEmailController;
-use Src\Application\Middlewares\RedirectUserLoggedMiddleware;
-use Src\Application\Controllers\SignInController;
-use Src\Application\Controllers\VerifyEmailViewController;
-use Src\Application\Middlewares\RedirectUserNotLoggedMiddleware;
-use Src\Controllers\SignInViewController;
-# use Src\Application\Controllers\ViewEventsController;
-use Src\Application\Controllers\RegisterEventController;
-use Src\Application\Controllers\ViewEventController;
-use Src\Application\Controllers\ViewEventsController;
+   use Dotenv\Dotenv;
+   use Src\Application\Routes\Router;
+   use Src\Application\Controllers\RegisterEventController;
+   use Src\Application\Controllers\ViewEventsController;
+// use Src\Application\Controllers\HomeController;
+// use Src\Application\Controllers\CreateUserController;
+// use Src\Application\Controllers\SignUpController;
+// use Src\Application\Controllers\SignUpViewController;
+// use Src\Application\Controllers\CreatePasswordController;
+// use Src\Application\Controllers\VerifyEmailController;
+// use Src\Application\Middlewares\RedirectUserLoggedMiddleware;
+// use Src\Application\Controllers\SignInController;
+// use Src\Application\Controllers\VerifyEmailViewController;
+// use Src\Application\Middlewares\RedirectUserNotLoggedMiddleware;
+// use Src\Controllers\SignInViewController;
+
 
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../../..");
 $dotenv->load();
@@ -37,18 +38,18 @@ $dotenv->load();
 $router = new Router();
 
 # API Routes
-$router->post('/api/v1/auth/signup', SignUpController::class);
-$router->post("/api/v1/signup/password", CreateUserController::class, RedirectUserLoggedMiddleware::class);
-$router->post('/api/v1/auth/signin', SignInController::class);
-$router->post("/api/v1/register/event", RegisterEventController::class);
+   $router->post("/api/v1/register/event", RegisterEventController::class);
+// $router->post('/api/v1/auth/signup', SignUpController::class);
+// $router->post("/api/v1/signup/password", CreateUserController::class, RedirectUserLoggedMiddleware::class);
+// $router->post('/api/v1/auth/signin', SignInController::class);
 
 # Views Routes
-$router->get('/home', HomeController::class, RedirectUserNotLoggedMiddleware::class);
-$router->get('/auth/signin', SignInViewController::class, RedirectUserLoggedMiddleware::class);
-$router->get("/auth/signup", SignUpViewController::class, RedirectUserLoggedMiddleware::class);
-$router->get("/auth/signup/password", CreatePasswordController::class, RedirectUserLoggedMiddleware::class);
-$router->get("/auth/signup/verify-email", VerifyEmailViewController::class);
-$router->get("/api/v1/auth/signup/verify-email", VerifyEmailController::class);
-$router->get("/api/v1/view/events", ViewEventsController::class);
+   $router->get("/api/v1/view/events", ViewEventsController::class);
+// $router->get('/home', HomeController::class, RedirectUserNotLoggedMiddleware::class);
+// $router->get('/auth/signin', SignInViewController::class, RedirectUserLoggedMiddleware::class);
+// $router->get("/auth/signup", SignUpViewController::class, RedirectUserLoggedMiddleware::class);
+// $router->get("/auth/signup/password", CreatePasswordController::class, RedirectUserLoggedMiddleware::class);
+// $router->get("/auth/signup/verify-email", VerifyEmailViewController::class);
+// $router->get("/api/v1/auth/signup/verify-email", VerifyEmailController::class);
 
 $router->run();
