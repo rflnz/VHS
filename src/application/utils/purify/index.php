f9f31cbff947dd4465ec88b18c6083c2fe54d285
____x_ <josuealexandre067@gmail.com>
2025-08-22 12:35:11 -0400

    feat/purifyDateTime para cards

--- src/application/utils/purify/index.php
+++ src/application/utils/purify/index.php
@@ -2,6 +2,29 @@
 
 namespace Src\Application\Utils\Purify;
 
+use DateTime;
+
 function purifyProperty($property) {
+    if (is_int($property) || is_float($property)) {
+        return $property;
+    }
+
     return htmlspecialchars(strip_tags($property), ENT_QUOTES, 'UTF-8');
+}
+
+function purifyDateTime(DateTime $date) : string {
+    if ($date instanceof \DateTime) {
+        return $date->format('d/m/Y H:i:s');
+    }
+
+    if (is_string($date)) {
+        try {
+            $newDate = new \DateTime($date);
+            return $newDate->format('d/m/Y H:i:s');
+        } catch (\Exception $e) {
+            return (new \DateTime())->format('d/m/Y H:i:s');
+        }
+    }
+
+    return (new \DateTime())->format('d/m/Y H:i:s');
 }
