c63e9e21d6da52617ea7190fd9a1fc0c08b65d85
____x_ <josuealexandre067@gmail.com>
2025-08-22 14:15:51 -0400

    .

--- src/views/pages/home/events/index.php
+++ src/views/pages/home/events/index.php
@@ -74,7 +74,7 @@
                     </div>
 
                     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                        <?= renderCards($events); ?>   
+                        <?= renderCards($events, 'event'); ?>   
                     </div>
                 </section>
             </div>
