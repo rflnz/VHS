5e63da774a9a020f45e206cbf001f3b07e9bcf92
____x_ <josuealexandre067@gmail.com>
2025-08-22 12:35:25 -0400

    feat/novos cards de exemplo

--- src/views/components/cards/exampleCards.php
+++ src/views/components/cards/exampleCards.php
@@ -1,56 +1,50 @@
 <?php
 
-    $cards = [
-        [
-            "type_card" => "video",
-            "title" => "Como aprender programação do zero e se tornar um excelente desenvolvedor full stack",
-            "duration" => "7 min",
-            "username" => "Rafael Germinari",
-            "thumbnail_url" => "https://t.ctcdn.com.br/69rFkwz-cdviPGZn2p_l6rJH0UA=/1200x675/smart/i533291.png",
-            "avatar_url" => "https://senachub.ms.senac.br/hubinnovation/uploads/fotos/6706850e20f59.jpg",
-            "views" => "53k",
-            "created_at" => "há 2 dias",
-            "url" => "#"
-        ],
-        
-        [
-            "type_card" => "event",
-            "title" => "Como aprender programação do zero e se tornar um excelente desenvolvedor full stack",
-            "duration" => "🔥",
-            "username" => "Rafael Germinari",
-            "thumbnail_url" => "https://framerusercontent.com/images/TO1bOWR2ihsAvIgtbf5Y9taYWZs.png",
-            "avatar_url" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS2EntOCdE0yEaIfacfxiU1ZyRi8RSeT-eu_HDeQSq6J_veZZesXpwlcxkWxM2NKMpWRb4CRyw9WdUGOQV7ZqK8g",
-            "views" => "1.1M",
-            "created_at" => "há 2 dias",
-            "url" => "#",
-            "description" => "Gratuito",
-            "maked_for" => "09/08 17:15"
-        ],
+$cards = [
+    "video" => [
+        "url" => "#",
+        "title" => "Como aprender programação do zero e se tornar um excelente desenvolvedor full stack",
+        "duration" => "7 min",
+        "username" => "Rafael Germinari",
+        "thumbnail_url" => "https://t.ctcdn.com.br/69rFkwz-cdviPGZn2p_l6rJH0UA=/1200x675/smart/i533291.png",
+        "avatar_url" => "https://senachub.ms.senac.br/hubinnovation/uploads/fotos/6706850e20f59.jpg",
+        "views" => "53k",
+        "created_at" => "há 2 dias"
+    ],
+    
+    "event" => [
+        "url" => "#",
+        "title" => "Como aprender programação do zero e se tornar um excelente desenvolvedor full stack",
+        "duration" => "🔥",
+        "username" => "Rafael Germinari",
+        "thumbnail_url" => "https://framerusercontent.com/images/TO1bOWR2ihsAvIgtbf5Y9taYWZs.png",
+        "avatar_url" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS2EntOCdE0yEaIfacfxiU1ZyRi8RSeT-eu_HDeQSq6J_veZZesXpwlcxkWxM2NKMpWRb4CRyw9WdUGOQV7ZqK8g",
+        "views" => "1.1M",
+        "created_at" => "há 2 dias",
+        "description" => "Gratuito",
+        "planned_event" => "09/08 17:15"
+    ],
 
-        [
-            "type_card" => "channel",
-            "title" => "Como aprender programação do zero e se tornar um excelente desenvolvedor full stack",
-            "duration" => "7 min",
-            "username" => "Rafael Germinari",
-            "thumbnail_url" => "https://t.ctcdn.com.br/69rFkwz-cdviPGZn2p_l6rJH0UA=/1200x675/smart/i533291.png",
-            "avatar_url" => "https://senachub.ms.senac.br/hubinnovation/uploads/fotos/6706850e20f59.jpg",
-            "created_at" => "há 1 ano",
-            "url" => "#",
-            "comments" => "12",
-            "likes" => "4.5",
-            "views" => "540K"
-        ],
+    "mychannel" => [
+        "url" => "#",
+        "title" => "Como aprender programação do zero e se tornar um excelente desenvolvedor full stack",
+        "duration" => "7 min",
+        "username" => "Rafael Germinari",
+        "thumbnail_url" => "https://t.ctcdn.com.br/69rFkwz-cdviPGZn2p_l6rJH0UA=/1200x675/smart/i533291.png",
+        "avatar_url" => "https://senachub.ms.senac.br/hubinnovation/uploads/fotos/6706850e20f59.jpg",
+        "created_at" => "há 1 ano",
+        "comments" => "12",
+        "likes" => "4.5",
+        "views" => "540K"
+    ],
 
-        [
-            "type_card" => "channels",
-            "thumbnail_url" => "https://t.ctcdn.com.br/69rFkwz-cdviPGZn2p_l6rJH0UA=/1200x675/smart/i533291.png",
-            "title" => "Como aprender programação do zero e se tornar um excelente desenvolvedor full stack",
-            "duration" => "7 min",
-            "username" => "Rafael Germinari",
-            "views" => "53k",
-            "created_at" => "2 dias atrás",
-            "url" => "#"
-        ]
-    ];
-
-?>
+    "channels" => [
+        "url" => "#",
+        "thumbnail_url" => "https://t.ctcdn.com.br/69rFkwz-cdviPGZn2p_l6rJH0UA=/1200x675/smart/i533291.png",
+        "title" => "Como aprender programação do zero e se tornar um excelente desenvolvedor full stack",
+        "duration" => "7 min",
+        "username" => "Rafael Germinari",
+        "views" => "53k",
+        "created_at" => "2 dias atrás"
+    ]
+];
