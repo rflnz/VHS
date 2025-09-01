6eaffcae8a66cc38c53160da2da9825216c04d6c
____x_ <josuealexandre067@gmail.com>
2025-09-01 17:09:45 -0400

    ref/trocando o nome do arquivo por conflito com "script" e adicionando o elemento .categories-title

--- src/views/components/sidebar/sidebar.js
+++ src/views/components/sidebar/sidebar.js
@@ -1,8 +1,9 @@
-const texts = document.querySelectorAll(".menu-text");
 const toggleButton = document.querySelector("#barrinha");
 const sidebar = document.querySelector("aside");
+const separator = document.querySelector(".separator");
+const title = document.querySelector(".categories-title");
+const texts = document.querySelectorAll(".menu-text");
 const icons = document.querySelectorAll(".icon");
-const separators = document.querySelectorAll(".separator");
 
 let isExpanded = true;
 
@@ -26,31 +27,28 @@ texts.forEach(text => {
     text.style.whiteSpace = "nowrap";
 });
 
-
 const url = window.location.href;
-
 url.includes("minimized") ? toggleSidebar(false) : null;
-
 
 function checkRoute() {
     const urlSplit = url.split("/home");
-
     icons[0].classList.toggle("active", url.includes("/home") && urlSplit.at(-1) === "/" || urlSplit.at(-1) === "");
     icons[1].classList.toggle("active", url.includes("/home/fast"));
     icons[2].classList.toggle("active", url.includes("/home/events"));
-    icons[3].classList.toggle("active", url.includes("/home/history"));   
+    icons[3].classList.toggle("active", url.includes("/home/history"));
+
     icons[4].classList.toggle("active", url.includes("category=tecnologia"));
     icons[5].classList.toggle("active", url.includes("category=saude"));
     icons[6].classList.toggle("active", url.includes("category=moda"));
     icons[7].classList.toggle("active", url.includes("category=estetica"));
-}
-
-checkRoute();
+} checkRoute();
 
 function toggleSidebar(state) {
     isExpanded = state !== undefined ? state : !isExpanded;
 
     sidebar.style.width = isExpanded ? "9.3rem" : "5.35rem";
+    separator.style.width = isExpanded ? "auto" : "2rem";
+    title.style.display = isExpanded ? "block" : "none";
 
     texts.forEach(text => {
         if (isExpanded) {
@@ -58,13 +56,9 @@ function toggleSidebar(state) {
             text.style.transform = "translateX(0)";
             return;
         }
+
         text.style.opacity = "0";
         text.style.transform = "translateX(-10px)";
-       
-    });
-
-    separators.forEach(separator => {
-        separator.style.width = isExpanded ? "auto" : "2rem";
     });
 }
 
